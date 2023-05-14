18183efb17716bb44e7a7ce2fa7597b7e7af39c5
Skoh <skookie.omg.lol@gmail.com>
2023-05-14 09:03:38 +0200

    patched some osef stuff

--- primitives/core.c
+++ primitives/core.c
@@ -37,11 +37,9 @@ int createWindow(SDL_Window **window, SDL_Renderer **renderer){
 int closeSDL(SDL_Window *window, SDL_Renderer *renderer){
 	if(NULL != renderer){ // Destroy renderer here
 		SDL_DestroyRenderer(renderer);
-		free(renderer);
 	}	 else { return EXIT_FAILURE; }
 	if(NULL != window){ // Destroy window here
 		SDL_DestroyWindow(window);
-		free(window);
 		SDL_Quit(); // Closes here
 		printf("Quit\n");
 		return EXIT_SUCCESS;
