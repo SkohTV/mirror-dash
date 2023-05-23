47fcdadebfa20bbd3120fa51f8130ea27040b565
Skoh <skookie.omg.lol@gmail.com>
2023-05-23 10:45:48 +0200

    up

---
+++ interfaces/mainMenu.c
@@ -0,0 +1,6 @@
+#include "mainMenu.h"
+
+
+void test(){
+
+}
---
+++ interfaces/mainMenu.h
@@ -0,0 +1,16 @@
+// Define for safety
+#ifndef MAINMENU_H
+#define MAINMENU_H
+
+// Includes & Defines
+#include "../primitives/_structs.h"
+
+
+// Functions
+/***/
+void test();
+
+
+
+// Close safety define
+#endif
